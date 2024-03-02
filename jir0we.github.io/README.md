af8dd270f06a1ebd06edbfb69737532ce47ea745
SilverShorthair <acooperw2018@gmail.com>
2024-03-01 23:55:47 -0700

    Chumbox Changes Update
    Note to self: Steal Jemma's update naming format

---
+++ jir0we.github.io/README.md
@@ -0,0 +1,3 @@
+# jir0we.github.io
+# you know, this is a website, and this is a readme for that website
+# ***what are thee doing here?***
---
+++ jir0we.github.io/js.js
@@ -0,0 +1,7 @@
+//var hamburger = document.querySelector(".navBurger");
+//hamburger.addEventListener("click", function(){
+//    document.querySelector("body").classList.toggle("active");
+//})
+function collapse() {
+    document.getElementById("sidebar").classList.toggle("collapse");
+}
